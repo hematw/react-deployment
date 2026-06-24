eb0a086489f5d2966c44ba2758816da0c8dbb791
Hematullah Waziri <hematullahw@gmail.com>
2026-06-24 21:28:17 +0430

    test deployment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text center text-emerald-400 text-5xl font-bold">
+        This is a test Deployment
+      </h1>
+    </div>
+  );
+}
+
+export default App;
